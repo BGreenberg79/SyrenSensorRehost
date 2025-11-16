1e419999b9a72eb5fd1a5e127c1857ed549f8f17
BGreenberg79 <greenbeb2@gmail.com>
2025-11-15 23:56:19 -0500

    schema update

--- amplify/data/schema.ts
+++ amplify/data/schema.ts
@@ -1,37 +1,76 @@
+
 import { a, type ClientSchema } from "@aws-amplify/backend";
 
 export const schema = a.schema({
-  Todo: a
+  Users: a
     .model({
-      content: a.string(),
+      email: a.string(),
+      userId: a.string(),
+      createdAt: a.string(),
+      updatedAt: a.string(),
     })
-    .authorization((allow) => [allow.owner()]),
-
-  HealthSnapshot: a
-    .model({
-      userID: a.id(),
-      heartRate: a.integer(),
-      oxygenLevel: a.integer(),
-      timestamp: a.timestamp(),
-    })
-    .authorization((allow) => [allow.owner()])
-    .secondaryIndexes((index) => [
-      index("userID").sortKeys(["timestamp"]),
-    ]),
+    .identifier(["email"])
+    .authorization((allow) => [allow.authenticated()]),
 
 
-  WearableRawData: a
+  UserProfiles: a
     .model({
-      userID: a.id(),
-      timestamp: a.timestamp(),
-      rawHeartRate: a.integer(),
-      rawO2: a.integer(),
-      processed: a.boolean(),
+      userId: a.string(),
+      email: a.string(),
+      firstName: a.string(),
+      lastName: a.string(),
+      age: a.integer(),
+      gender: a.string(),
+      height: a.string(),
+      weight: a.integer(),
+      phoneNumber: a.string(),
+      emergencyContactId: a.string(),
+      addressId: a.string(),
+      createdAt: a.string(),
     })
-    .authorization((allow) => [allow.owner()])
-    .secondaryIndexes((index) => [
-      index("userID").sortKeys(["timestamp"]),
-    ]),
+    .identifier(["userId"])
+    .authorization((allow) => [allow.authenticated()]),
+
+  EmergencyContacts: a
+    .model({
+      contactId: a.string(),
+      userId: a.string(),
+      firstName: a.string(),
+      lastName: a.string(),
+      phoneNumber: a.string(),
+      relationship: a.string(),
+      createdAt: a.string(),
+    })
+    .identifier(["contactId"])
+    .authorization((allow) => [allow.authenticated()]),
+
+  Addresses: a
+    .model({
+      addressId: a.string(),
+      userId: a.string(),
+      buildingNumber: a.string(),
+      street: a.string(),
+      aptUnitNumber: a.string(),
+      city: a.string(),
+      state: a.string(),
+      zipCode: a.string(),
+      country: a.string(),
+      createdAt: a.string(),
+    })
+    .identifier(["addressId"])
+    .authorization((allow) => [allow.authenticated()]),
+
+  Vitals: a
+    .model({
+      userId: a.string(),
+      timestamp: a.integer(),
+      skinTemp: a.integer(),
+      pulse: a.integer(),
+      spO2: a.integer(),
+      createdAt: a.string(),
+    })
+    .identifier(["userId", "timestamp"])
+    .authorization((allow) => [allow.authenticated()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
